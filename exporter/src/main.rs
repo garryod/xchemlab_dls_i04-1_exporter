8fe8d840074299d49bcc48256135a68cd801de64
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2023-04-24 15:39:26 +0000

    Add schema cli command

--- exporter/src/main.rs
+++ exporter/src/main.rs
@@ -10,8 +10,28 @@ use axum::{
     routing::get,
     Extension, Router, Server,
 };
-use sea_orm::Database;
+use clap::Parser;
+use sea_orm::{Database, DatabaseConnection};
 use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
+
+async fn setup_database() -> DatabaseConnection {
+    let database_url = std::env::var("DATABASE_URL").expect("");
+    Database::connect(&database_url)
+        .await
+        .expect("The DATABASE_URL environment variable must point to a live instance of ISPyB")
+}
+
+async fn setup_api(
+    database: DatabaseConnection,
+) -> Schema<RootQuery, RootMutation, RootSubscription> {
+    Schema::build(
+        RootQuery::default(),
+        RootMutation::default(),
+        RootSubscription::default(),
+    )
+    .data(database)
+    .finish()
+}
 
 async fn graphiql() -> impl IntoResponse {
     response::Html(
@@ -26,30 +46,52 @@ async fn graphql_handler(schema: Extension<RootSchema>, req: GraphQLRequest) -> 
     schema.execute(req.into_inner()).await.into()
 }
 
-#[tokio::main]
-async fn main() {
-    let database_url = std::env::var("DATABASE_URL").expect("");
-    let database = Database::connect(&database_url)
-        .await
-        .expect("The DATABASE_URL environment variable must point to a live instance of ISPyB");
-
-    let schema = Schema::build(
-        RootQuery::default(),
-        RootMutation::default(),
-        RootSubscription::default(),
-    )
-    .data(database)
-    .finish();
-
-    let app = Router::new()
+async fn setup_router<Q, M, S>(schema: Schema<Q, M, S>) -> Router
+where
+    Q: async_graphql::ObjectType + 'static,
+    M: async_graphql::ObjectType + 'static,
+    S: async_graphql::SubscriptionType + 'static,
+{
+    Router::new()
         .route("/", get(graphiql).post(graphql_handler))
         .route_service("/ws", GraphQLSubscription::new(schema.clone()))
-        .layer(Extension(schema));
+        .layer(Extension(schema))
+}
 
+async fn serve(router: Router) {
     let socket_addr: SocketAddr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 13245));
     println!("GraphiQL IDE: {}", socket_addr);
     Server::bind(&socket_addr)
-        .serve(app.into_make_service())
+        .serve(router.into_make_service())
         .await
         .unwrap();
+}
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+enum Cli {
+    /// Starts a webserver serving the GraphQL API
+    Serve,
+    /// Prints the GraphQL API to stdout
+    Schema,
+}
+
+#[tokio::main]
+async fn main() {
+    let args = Cli::parse();
+
+    match args {
+        Cli::Serve => {
+            let database = setup_database().await;
+            let schema = setup_api(database).await;
+            let router = setup_router(schema).await;
+            serve(router).await;
+        }
+        Cli::Schema => {
+            let database = setup_database().await;
+            let schema = setup_api(database).await;
+            let schema_string = schema.sdl();
+            println!("{}", schema_string);
+        }
+    }
 }
