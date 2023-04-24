97fb2a53330b668a9fd1c451e61852ba154bc69a
Garry O'Donnell <garry.o'donnell@diamond.ac.uk>
2023-04-24 16:52:02 +0000

    Add basic create shipment mutation

--- exporter/src/api/mod.rs
+++ exporter/src/api/mod.rs
@@ -2,19 +2,18 @@ mod person;
 mod proposals;
 mod shipment;
 
-use self::person::PersonQuery;
 use self::proposals::ProposalQuery;
 use self::shipment::ShipmentQuery;
-use async_graphql::{EmptyMutation, EmptySubscription, Enum, MergedObject, Schema};
+use self::{person::PersonQuery, shipment::ShipmentMutation};
+use async_graphql::{EmptySubscription, Enum, MergedObject, Schema};
 
 pub type RootSchema = Schema<RootQuery, RootMutation, RootSubscription>;
 
 #[derive(Debug, MergedObject, Default)]
 pub struct RootQuery(PersonQuery, ProposalQuery, ShipmentQuery);
 
-// #[derive(Debug, MergedObject, Default)]
-// pub struct RootMutation(ShipmentMutation);
-pub type RootMutation = EmptyMutation;
+#[derive(Debug, MergedObject, Default)]
+pub struct RootMutation(ShipmentMutation);
 
 #[derive(Debug, Enum, PartialEq, Eq, Clone, Copy)]
 pub enum MutationType {
